abad04065a6bda525199ba408b90b1b1d6c6bc28
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-14 16:20:38 -0600

    Se configuro la dependencia de tailwind y vue router en main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Use tailwind
+import '@/assets/css/tailwind.css'
+//Use own css
+import '@/assets/css/app.css'
+
+//import router from router/index.js
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
