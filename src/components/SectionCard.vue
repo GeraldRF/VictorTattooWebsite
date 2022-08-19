d14002bf140168e29abf4ed7d49914056b6d8e87
Gerald Ramírez <gerald.rafo@gmail.com>
2022-08-19 05:34:41 -0600

    firebase y seccion component

---
+++ src/components/SectionCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="flex items-center justify-center gap-0 px-4 lg:px-8">
+            <div class="h-[0.1px] bg-gray-700 lg:w-5 grow shrink lg:grow-0 lg:shrink-0"></div>
+            <h2 class="flex justify-center items-center px-4 text-2xl grow-0 shrink-0 border-[1px] border-gray-700 rounded-xl shadow-md shadow-gray-900">{{name}}
+            </h2>
+            <div class="h-[0.1px] bg-gray-700 grow shrink"></div>
+        </div>
+        <div class="mx-auto py-8 w-11/12 relative">
+            <CarouselCard :data="data"></CarouselCard>
+        </div>
+        <div class="flex justify-end px-3 lg:px-8 mb-16">
+            <router-link :to="routerLink" class="mr-3 px-3 py-1 border-2 rounded-3xl hover:text-gray-300">Ver {{name}}</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import CarouselCard from "@/components/CarouselCard.vue";
+
+export default {
+    props: {
+        data: {
+            type: [Object, Array],
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        routerLink: {
+            type: String,
+            required: true
+        }
+    },
+    components: {
+        CarouselCard
+    }
+
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,12 @@
   <div class="text-white">
     <Header></Header>
     <div class="flex flex-col gap-4 mt-20">
-      <div>
-        <div class="flex items-center justify-center gap-0 px-4 lg:px-8">
-          <div class="h-px bg-white lg:w-5 grow shrink lg:grow-0 lg:shrink-0"></div>
-          <h2 class="flex justify-center items-center px-4 text-2xl grow-0 shrink-0 border-x-2 rounded-xl">Catalogo
-          </h2>
-          <div class="h-px bg-white grow shrink"></div>
-        </div>
-        <div class="mx-auto py-8 w-11/12 relative">
-          <CarouselCard :data="tattos"></CarouselCard>
-        </div>
-        <div class="flex justify-end px-3 lg:px-8 mb-16">
-          <router-link to="/catalogue" class="mr-3 px-3 py-1 border-2 rounded-3xl hover:text-gray-300">Ver catalogo
-            completo</router-link>
-        </div>
-      </div>
+
+      <SectionCard name="Catalogo" :data="tattos" router-link="/catalogue"></SectionCard>
+
+      <SectionCard name="Promociones" :data="tattos" router-link="/catalogue"></SectionCard>
+
+      <SectionCard name="Trabajos" :data="tattos" router-link="/catalogue"></SectionCard>
 
       <div>
         <div class="flex items-center justify-center gap-3 px-4 lg:px-8">
@@ -79,6 +70,7 @@
 <script>
 import Header from "@/components/header/Header.vue";
 import CarouselCard from "@/components/CarouselCard.vue";
+import SectionCard from "@/components/SectionCard.vue";
 
 import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from '@heroicons/vue/solid'
 import { ref } from 'vue';
@@ -89,13 +81,15 @@ import useBreakpoints from "@/composables/useBreakpoints";
 
 
 
+
 export default {
   components: {
     Header,
     ArrowCircleLeftIcon,
     ArrowCircleRightIcon,
-    CarouselCard
-  },
+    CarouselCard,
+    SectionCard
+},
   setup() {
     const tattos = ref(availableTattos);
 
