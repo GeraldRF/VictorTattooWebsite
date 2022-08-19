892aa5d77d2ac7233406fac2b27912f23fbb21c1
Gerald Ramírez <gerald.rafo@gmail.com>
2022-08-19 05:45:09 -0600

    prueba

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-import { createApp } from 'vue'
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+import {
+    getAnalytics
+} from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDFaLwfy7M000L_CZs8bt1teRl2pF-ch5U",
+    authDomain: "victortattoowebsite.firebaseapp.com",
+    projectId: "victortattoowebsite",
+    storageBucket: "victortattoowebsite.appspot.com",
+    messagingSenderId: "229461931182",
+    appId: "1:229461931182:web:1b6cd8636fa69c14f3693f",
+    measurementId: "G-3LNR27Z63Z"
+};
+
+// Initialize Firebase
+const app2 = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
 
 //Use tailwind
@@ -12,4 +41,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(app2)
 app.mount('#app')
