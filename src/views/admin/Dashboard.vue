2c890438c1b45cb07dbad302fe9bb43cea3864f0
Gerald <gerald@4thewords.com>
2023-01-05 08:47:51 -0600

    push

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -15,6 +15,17 @@ export default {
   name: "Dashboard",
   setup() {
     onMounted(() => {
+      //attributes.src.value = https://script.hotjar.com/modules.352fddba5b21bbfc3a08.js
+      //https://static.hotjar.com/c/hotjar-3309748.js?sv=6
+      let script1 = document.head.children.find(child => child.attributes.src.value === 'https://script.hotjar.com/modules.352fddba5b21bbfc3a08.js');
+      let script2 = document.head.children.find(child => child.attributes.src.value === 'https://static.hotjar.com/c/hotjar-3309748.js?sv=6');
+      if(script1){
+        document.head.removeChild(script1);
+      }
+      if(script2){
+        document.head.removeChild(script2);
+      }
+      
       console.log(document.head.children, document.head);
     });
   }
